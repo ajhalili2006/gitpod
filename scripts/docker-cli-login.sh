694b9abdd9744f267316a0458c056ed9bcb65c11
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2021-12-27 11:17:19 +0000

    Add fixes to docker-cli-login script
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- scripts/docker-cli-login.sh
+++ scripts/docker-cli-login.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ $GITPOD_INSTANCE_ID != "" ]]; then
+  eval $(gp env -e)
+fi
+
+if [[ $RHQCR_USERNAME == "" || $RHQCR_PASSWORD == "" ]]; then
+  echo "Missing container registry login info, skipping..."
+  exit
+fi
+
 docker login -u "$RHQCR_USERNAME" -p "$RHQCR_PASSWORD" quay.io
