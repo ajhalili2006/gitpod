71a16c550b931ec98bb3789570d2316b228bfd1a
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2022-01-03 13:16:35 +0000

    Update tasks to include both upstream and GitHub fork remote
    Also updated the authenicate-uplink-cli script to simply process,
    among other improvements.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- scripts/authenicate-uplink-cli.sh
+++ scripts/authenicate-uplink-cli.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/bash
 # shellcheck shell=bash
 
+GP_LOCALAPP_BUCKET_ACCESS_GRANT=${GP_LOCALAPP_BUCKET_ACCESS_GRANT:-$1}
+
+if ! command -v uplink>>/dev/null; then
+  echo "error: Storj Uplink doesn't exist on PATH, perhaps not installed? Check https://docs.storj.io/dcs/downloads/download-uplink-cli"
+  echo "error: for the install guide."
+fi
+
 if [[ $GITPOD_INSTANCE_ID == "" ]] && [[ $1 == "" ]]; then
   echo "Usage: $0 [STORJ DCS ACCESS GRANT]"
+  echo
+  echo "Tip: Use GP_LOCALAPP_BUCKET_ACCESS_GRANT variable to set this instead of passing it as an script argument,"
+  echo "     through risking your access grant against unauthorized access."
 fi
 
-if [[ $GP_LOCALAPP_BUCKET_ACCESS_GRANT == "" &&  $GITPOD_INSTANCE_ID != "" ]]; then
-  echo "Missing access grant on variable GP_LOCALAPP_BUCKET_ACCESS_GRANT, skipping..."
-fi
-
+# TODO: Revisit uplink import help stuff because force flag sunds kinda sussy.
 if [[ $GITPOD_INSTANCE_ID != "" ]]; then
-  "$GITPOD_REPO_ROOT/scripts/gp-uplink" import "$GP_LOCALAPP_BUCKET_ACCESS_GRANT"
+  "$GITPOD_REPO_ROOT/scripts/gp-uplink" import "$GP_LOCALAPP_BUCKET_ACCESS_GRANT" --force
 else
-   uplink import $1
+   uplink import $GP_LOCALAPP_BUCKET_ACCESS_GRANT --force
 fi
